18fd9d74d667cf251ac535757d51f2e3431ba8b9
calm-4 <calmshop4@gmail.com>
2026-05-12 04:24:52 +0200

    Integrate Discord.js client into index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Bot läuft");
-});
-
-app.listen(3000, () => {
-  console.log("Webserver läuft");
-});
-
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-client.once("ready", () => {
+client.once('ready', () => {
   console.log(`Eingeloggt als ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
 });
 
 client.login(process.env.TOKEN);
