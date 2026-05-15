fafa97d86f888c07d739896e81b82dc0a3ad6d94
calm-4 <calmshop4@gmail.com>
2026-05-15 15:36:30 +0200

    Add roll command to generate random dice rolls

--- commands/Fun/roll.js
+++ commands/Fun/roll.js
@@ -1,1 +1,23 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Würfle eine Zahl'),
+
+    async execute(interaction) {
+
+        const zahl = Math.floor(Math.random() * 6) + 1;
+
+        const embed = new EmbedBuilder()
+            .setColor('#57F287')
+            .setTitle('🎲 Würfel')
+            .setDescription(`Du hast **${zahl}** gewürfelt!`)
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
