a0107612f942aa56cc36b6deba2510cb15be9deb
calm-4 <calmshop4@gmail.com>
2026-05-15 03:05:39 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,36 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
 
+const commands = [];
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file =>
+  file.endsWith('.js')
+);
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST().setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+
+    console.log('🔄 Slash Commands werden geladen...');
+
+    await rest.put(
+      Routes.applicationCommands('DEINE_CLIENT_ID'),
+      { body: commands },
+    );
+
+    console.log('✅ Slash Commands geladen');
+
+  } catch (error) {
+    console.error(error);
+  }
+})();
