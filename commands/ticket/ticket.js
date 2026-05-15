f1d5873d72967074984222337a180f63e1ffb92f
calm-4 <calmshop4@gmail.com>
2026-05-15 15:43:32 +0200

    Update ticket.js

--- commands/ticket/ticket.js
+++ commands/ticket/ticket.js
@@ -1,1 +1,38 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require('discord.js');
 
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('ticket')
+        .setDescription('Ticket Panel'),
+
+    async execute(interaction) {
+
+        const embed = new EmbedBuilder()
+            .setColor('#5865F2')
+            .setTitle('🎫 Support Tickets')
+            .setDescription(
+                'Klicke unten um ein Ticket zu erstellen.'
+            );
+
+        const button = new ButtonBuilder()
+            .setCustomId('ticket')
+            .setLabel('🎫 Ticket erstellen')
+            .setStyle(ButtonStyle.Primary);
+
+        const row =
+            new ActionRowBuilder()
+                .addComponents(button);
+
+        interaction.reply({
+            embeds: [embed],
+            components: [row]
+        });
+    }
+};
