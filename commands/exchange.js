6b982821e2b0bdba95aa04e79d42cd85aad080db
calm-4 <calmshop4@gmail.com>
2026-05-15 15:24:09 +0200

    Update exchange.js

--- commands/exchange.js
+++ commands/exchange.js
@@ -1,1 +1,53 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder
+} = require('discord.js');
 
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('exchange')
+        .setDescription('Währungen umrechnen')
+        .addNumberOption(option =>
+            option
+                .setName('betrag')
+                .setDescription('Betrag')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('von')
+                .setDescription('Von')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('zu')
+                .setDescription('Zu')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        const betrag = interaction.options.getNumber('betrag');
+        const von = interaction.options.getString('von').toUpperCase();
+        const zu = interaction.options.getString('zu').toUpperCase();
+
+        const res = await axios.get(
+            `https://api.exchangerate.host/convert?from=${von}&to=${zu}&amount=${betrag}`
+        );
+
+        const result = res.data.result;
+
+        const embed = new EmbedBuilder()
+            .setColor('#FEE75C')
+            .setTitle('💱 Currency Exchange')
+            .setDescription(
+                `💸 **${betrag} ${von}** = **${result.toFixed(2)} ${zu}**`
+            )
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    }
+};
