0a9204231967869718f1f232e7ea4592d176d487
calm-4 <calmshop4@gmail.com>
2026-05-15 03:00:25 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Antwortet mit Pong!'),
+
+  async execute(interaction) {
+    await interaction.reply('🏓 Pong!');
+  },
+};
